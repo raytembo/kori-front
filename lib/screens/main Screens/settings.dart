0e3d73b33ed606d77d36240ae80b197cfe564925
Ctrl+Ray <119338535+raytembo@users.noreply.github.com>
2024-09-02 15:42:17 +0200

    changed color for the app bar on the settings screen

--- lib/screens/main Screens/settings.dart
+++ lib/screens/main Screens/settings.dart
@@ -5,64 +5,68 @@ class SettingsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Settings'),
-        backgroundColor: Colors.white,
+        title: const Text('Settings',
+          style: TextStyle(
+            fontWeight: FontWeight.bold
+          ),
+        ),
+        backgroundColor: Colors.teal,
         elevation: 0,
       ),
       body: ListView(
         children: [
           ListTile(
-            title: Text('Your Profile'),
+            title: const Text('Your Profile'),
             onTap: () {
               // Navigate to Profile Screen
             },
           ),
           ListTile(
-            title: Text('Saved Tags'),
+            title: const Text('Saved Tags'),
             onTap: () {
               // Navigate to Saved Tags Screen
             },
           ),
           ListTile(
-            title: Text('Saved Cards'),
+            title: const Text('Saved Cards'),
             onTap: () {
               // Navigate to Saved Cards Screen
             },
           ),
           ListTile(
-            title: Text('Notifications & Emails'),
+            title: const Text('Notifications & Emails'),
             onTap: () {
               // Navigate to Notifications & Emails Screen
             },
           ),
-          Divider(),
+          const Divider(),
           ListTile(
-            title: Text('Application Password'),
+            title: const Text('Application Password'),
             onTap: () {
               // Navigate to Application Password Screen
             },
           ),
           ListTile(
-            title: Text('Verification OTP'),
+            title: const Text('Verification OTP'),
             onTap: () {
               // Navigate to Verification OTP Screen
             },
           ),
-          Divider(),
+          const Divider(),
           ListTile(
-            title: Text('Contact Support'),
+            title: const Text('Contact Support'),
             onTap: () {
               // Navigate to Contact Support Screen
             },
           ),
           ListTile(
-            title: Text('About Recharge Fastag'),
+            title: const Text('About Recharge Fastag'),
             onTap: () {
               // Navigate to About Recharge Fastag Screen
             },
           ),
           ListTile(
-            title: Text('Legal'),
+            title: const Text('Legal'),
             onTap: () {
               // Navigate to Legal Screen
             },
