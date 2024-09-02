7c6b2373efa0fe41bc3082a6f0bbf59b70eaca2b
Ctrl+Ray <119338535+raytembo@users.noreply.github.com>
2024-09-02 18:27:07 +0200

    fixed the calendar functionality

--- lib/screens/Other Screens/calendar.dart
+++ lib/screens/Other Screens/calendar.dart
@@ -9,40 +9,46 @@ class CalendarScreen extends StatefulWidget {
 }
 
 class _CalendarScreenState extends State<CalendarScreen> {
-  DateTime? _selectedDay;
   DateTime _focusDay = DateTime.now();
 
-  void setday(DateTime day, DateTime focusedDay){
+  void selectMethod(DateTime day, DateTime focusedDay){
     setState(() {
       _focusDay = day;
     });
   }
 
-
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      body:Padding(
-        padding: const EdgeInsets.only(top: 50.0),
-        child: TableCalendar(
-          availableGestures: AvailableGestures.all,
-          headerStyle: const HeaderStyle(formatButtonVisible: false,titleCentered: true),
-          locale: 'en_Us',
-          firstDay: DateTime.utc(2010, 10, 16),
-          lastDay: DateTime.utc(2030, 3, 14),
-          focusedDay: _focusDay,
-          selectedDayPredicate: (day)=>isSameDay(day, _focusDay),
-          onDaySelected: setday,
+      body:SafeArea(
+        child: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text("Schedule", style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 50.0),
+              child: TableCalendar(
+                availableGestures: AvailableGestures.all,
+                headerStyle: const HeaderStyle(formatButtonVisible: false,titleCentered: true),
+                locale: 'en_Us',
+                firstDay: DateTime.utc(2010, 10, 16),
+                lastDay: DateTime.utc(2030, 3, 14),
+                focusedDay: _focusDay,
+                selectedDayPredicate: (day)=>isSameDay(day, _focusDay),
+                onDaySelected: selectMethod,
+              ),
+            ),
+        
+          ],
+        
         ),
       )
     );
   }
-  void _onDaySelected(selectedDay, focusedDay){
-    if(isSameDay(_selectedDay, selectedDay)){
-      setState(() {
-        _selectedDay = selectedDay;
-        _focusDay = focusedDay;
-      });
-    }
-  }
+
 }
