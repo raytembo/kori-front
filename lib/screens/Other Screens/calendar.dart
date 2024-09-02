b9b5b2be7be7083421e5cd96fe6ca6d485081c6a
Ctrl+Ray <119338535+raytembo@users.noreply.github.com>
2024-09-02 18:15:21 +0200

    fixed the calendar functionality

--- lib/screens/Other Screens/calendar.dart
+++ lib/screens/Other Screens/calendar.dart
@@ -12,6 +12,12 @@ class _CalendarScreenState extends State<CalendarScreen> {
   DateTime? _selectedDay;
   DateTime _focusDay = DateTime.now();
 
+  void setday(DateTime day, DateTime focusedDay){
+    setState(() {
+      _focusDay = day;
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -19,12 +25,14 @@ class _CalendarScreenState extends State<CalendarScreen> {
       body:Padding(
         padding: const EdgeInsets.only(top: 50.0),
         child: TableCalendar(
-          locale: 'en_us',
+          availableGestures: AvailableGestures.all,
+          headerStyle: const HeaderStyle(formatButtonVisible: false,titleCentered: true),
+          locale: 'en_Us',
           firstDay: DateTime.utc(2010, 10, 16),
           lastDay: DateTime.utc(2030, 3, 14),
           focusedDay: _focusDay,
-          selectedDayPredicate: (day)=>isSameDay(_selectedDay, day),
-          onDaySelected: _onDaySelected,
+          selectedDayPredicate: (day)=>isSameDay(day, _focusDay),
+          onDaySelected: setday,
         ),
       )
     );
